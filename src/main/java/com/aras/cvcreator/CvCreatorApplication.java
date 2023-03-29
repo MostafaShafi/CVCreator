7a10a51be170e3b4be4f86e992138900ca252983
MostafaShafi <mostafa.shafi94@gmail.com>
2023-03-30 00:08:46 +0430

    Initial commit

---
+++ src/main/java/com/aras/cvcreator/CvCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.aras.cvcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvCreatorApplication.class, args);
+    }
+
+}
