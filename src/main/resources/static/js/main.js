2ff36a01c7580b1f7595e20603c78ca407a8edf2
shafi <m.shafi@douran.ir>
2023-11-28 14:45:17 -0800

    Refactor: use let in JavaScript methods

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,8 +5,8 @@ function emailValidation(emailAddress) {
 }
 
 function checkPasswordFormat() {
-    pass_format = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$/;
-    var pass = document.getElementById("password");
+    let pass_format = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$/;
+    let pass = document.getElementById("password");
 
     if (!pass.value.match(pass_format)) {
         alert("Password should be at least 8 character " +
@@ -15,11 +15,9 @@ function checkPasswordFormat() {
 }
 
 function checkPhoneNumber(phone) {
-    regex = /^\d{11}$/;
+    let regex = /^\d{11}$/;
     if (phone.value.match(regex) == null)
         alert("Please fill Phone with just numbers with length 11!");
-    /*if (phone.value.length != 11)
-        alert("Please fill Phone with just 11 character!");*/
 }
 
 function checkGradeRange(grade) {
@@ -30,7 +28,7 @@ function checkGradeRange(grade) {
 }
 
 function hideShowPassword() {
-    passwordType = document.getElementById("password").getAttribute("type") === "password" ? "text" : "password";
+    let passwordType = document.getElementById("password").getAttribute("type") === "password" ? "text" : "password";
     document.getElementById("password").setAttribute("type", passwordType);
 }
 
@@ -39,7 +37,7 @@ function makeExperienceEndDateRequired() {
 }
 
 function showErrorModal() {
-    errorMessage = document.getElementById("skills-error-message");
+    let errorMessage = document.getElementById("skills-error-message");
     if (errorMessage.innerHTML != null && errorMessage.innerHTML.length > 0) {
         alert(errorMessage.innerHTML);
     }
@@ -48,6 +46,6 @@ function showErrorModal() {
 function makePDF() {
     const element = document.getElementById('main');
     const personName = document.getElementById('PersonName');
-    var opt = {filename: personName.innerText + ' - Resume.pdf'};
+    let opt = {filename: personName.innerText + ' - Resume.pdf'};
     html2pdf().set(opt).from(element).save();
 }
